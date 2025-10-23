c26edd1114557a4654e4abf9084b9ee5c174075a
URVANSH AGARWAL <227296010+urvansh12-tech@users.noreply.github.com>
2025-10-23 23:59:15 +0530

    Update and rename Q1 -Write a program to input two numbers and display their sum to Day01_A_Add.c
    Write a program to input two numbers and display their sum.

---
+++ Day01_A_Add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+\\Write a program to input two numbers and display their sum
+
+int main()
+{
+    int a, b, sum;
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+    printf("Sum = %d\n", sum);
+
+  return 0;
+}
