6f185a9695420c315580ec96ed3016aa23e03082
URVANSH AGARWAL <227296010+urvansh12-tech@users.noreply.github.com>
2025-10-24 00:22:05 +0530

    Update and rename Q9 -Write a program to calculate simple and compound interest for given principal, rate, and time. to Day05_A_SI.c
    Write a program to calculate simple and compound interest for given principal, rate, and time.

--- Day05_A_SI.c
+++ Day05_A_SI.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main() {
+\\Write a program to calculate simple and compound interest for given principal, rate, and time
+
+int main() 
+ {
     float principal, rate, time;
 
     printf("Input 1:\n");
@@ -16,15 +19,3 @@ int main() {
 
     return 0;
 }
----------------------------------------------------------------------------------------------
-Input 1:
-1000 5 2
-Output 1:
-Simple Interest=100, Compound Interest=102.5
-
-Input 2:
-5000 7 3
-Output 2:
-Simple Interest=1050, Compound Interest=1125.76
-
-*/
