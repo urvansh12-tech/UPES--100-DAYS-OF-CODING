79adadeaf8b38a1ffc0013905a58a970c0ee9df3
URVANSH AGARWAL <227296010+urvansh12-tech@users.noreply.github.com>
2025-10-24 00:09:23 +0530

    Update and rename Q3-Write a program to calculate the area and perimeter of a rectangle given its length and breadth. to Day02_A_Perimeter.c
    Write a program to calculate the area and perimeter of a rectangle given its length and breadth

--- Day02_A_Perimeter.c
+++ Day02_A_Perimeter.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main() {
+\\Write a program to calculate the area and perimeter of a rectangle given its length and breadth
+
+int main() 
+ {
 	int length, breadth, area , perimeter;
 	    length = 5 , breadth = 10;
 	    
@@ -14,16 +17,3 @@ int main() {
 	     
 	return 0;
 }
-----------------------------------------------------------
-Sample Test Cases:
-Input 1:
-5 10
-Output 1:
-Area=50, Perimeter=30
-
-Input 2:
-3 7
-Output 2:
-Area=21, Perimeter=20
-
-*/
